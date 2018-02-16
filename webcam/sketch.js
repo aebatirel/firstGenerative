f903d5043c464a08f53704635b1cfeae9313ea7a
Ahmet <Ahmet@FZs-MacBook-Pro-2.local>
2018-02-17 00:12:36 +0300

    new shit got added

--- webcam/sketch.js
+++ webcam/sketch.js
@@ -1,15 +1,30 @@
 var capture;
 
 function setup() {
-  getUserMedia();
-  createCanvas(390, 240);
+  createCanvas(windowWidth,windowHeight);
   capture = createCapture(VIDEO);
-  capture.size(320, 240);
+  capture.size(width/2, height/2);
+  frameRate(120);
   //capture.hide();
 }
 
 function draw() {
-  //background(capture);
-  image(capture, 0, 0, 320, 240);
-  filter('INVERT');
+  for (let i = 0; i < 200; i++) {
+    getPixel();
+  }
+}
+function getPixel(){
+  var x=random()*width;
+  var y=random()*height;
+  var color=capture.get(x/2,y/2);
+  var red=color[0];
+  var green =color[1];
+  var blue = color[2];
+  var ratio = pow((red*green*blue),0.3)/255;
+  
+  fill(red,green,blue,75);
+  noStroke();
+  var radius = 40*(1-ratio);
+
+  ellipse(x,y,radius,radius);
 }
